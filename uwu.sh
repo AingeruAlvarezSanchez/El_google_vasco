a4047d9c3c5a5cbb1aaa46ac8c9e7168518c866b
Aingeru Alvarez sanchez <aalvarez@c3r2s3.42urduliz.com>
2023-03-14 18:36:54 +0100

    uwu2

--- uwu.sh
+++ uwu.sh
@@ -1,6 +1,6 @@
 afplay --volume 1 UwU.mp3 > result.txt &&
 
-while [ 1 ]
-do
-	afplay --volume 1 --rate 2.0 --rQuality 1 Despacito2_el_retorno.mp3 > result.txt && rm result.txt
-done 
+#while [ 1 ]
+#do
+#	afplay --volume 1 --rate 2.0 --rQuality 1 Despacito2_el_retorno.mp3 > result.txt && rm result.txt
+#done 
