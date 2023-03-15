964bbd2235ef9e72c4e79e58da63e9072c78ff5a
Aingeru <89106423+AingeruAlvarezSanchez@users.noreply.github.com>
2023-03-15 20:00:10 +0100

    UwUpdate

--- uwuinstal.sh
+++ uwuinstal.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/AingeruAlvarezSanchez/El_google_vasco.git ~/El_google_vasco
-echo "sh El_google_vasco/uwu.sh" >> ~/.zshrc
+echo "sh El_google_vasco/uwu.sh & ; clear" >> ~/.zshrc
 clear
